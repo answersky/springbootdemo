f2a28dfbcecd59b500a583afff6ceb7615ca23ba
liufeng <liufeng@111.com.cn>
2020-05-11 16:10:03 +0800

    slf4j+logback

--- src/main/java/com/answer/RunApplication.java
+++ src/main/java/com/answer/RunApplication.java
@@ -1,6 +1,8 @@
 package com.answer;
 
 import com.answer.spring_listener.SpringEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @SpringBootApplication
 public class RunApplication {
-
+    private static final Logger log=LoggerFactory.getLogger(RunApplication.class);
 
     public static void main(String[] args) {
 //        SpringApplication.run(RunApplication.class,args);
@@ -21,5 +23,6 @@ public class RunApplication {
         ConfigurableApplicationContext context = application.run(args);
         SpringEvent springEvent=new SpringEvent(context,"Spring事件");
         context.publishEvent(springEvent);
+        log.error("logback日志打印");
     }
 }
