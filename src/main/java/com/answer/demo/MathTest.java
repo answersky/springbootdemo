d0c9742ca9dd9d2b60a021071cfaf8f44c6a2b37
liufeng <liufeng@mingdutech.com>
2021-11-11 11:46:09 +0800

    条形码特殊符号处理

--- src/main/java/com/answer/demo/MathTest.java
+++ src/main/java/com/answer/demo/MathTest.java
@@ -3,6 +3,7 @@ package com.answer.demo;
 import org.assertj.core.util.Lists;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,12 +14,18 @@ import java.util.List;
  */
 public class MathTest {
     public static void main(String[] args) {
-       BigDecimal price= new BigDecimal("77").multiply(new BigDecimal(60)).divide(new BigDecimal(100),2,BigDecimal.ROUND_HALF_UP);
-        System.out.println(price);
+//       BigDecimal price= new BigDecimal("77").multiply(new BigDecimal(60)).divide(new BigDecimal(100),2,BigDecimal.ROUND_HALF_UP);
+//        System.out.println(price);
 
 
 //        List<BigDecimal> aa= Lists.newArrayList(new BigDecimal("1"),new BigDecimal("23"),null);
-        List<BigDecimal> aas= new ArrayList<>();
-        System.out.println(Collections.min(aas));
+//        List<BigDecimal> aas= new ArrayList<>();
+//        System.out.println(Collections.min(aas));
+
+        NumberFormat formatter = NumberFormat.getNumberInstance();
+        formatter.setMinimumIntegerDigits(3);
+        formatter.setGroupingUsed(false);
+        String result = formatter.format(10);
+        System.out.println(result);
     }
 }
--- src/main/java/com/answer/demo/barcode/Barcode.java
+++ src/main/java/com/answer/demo/barcode/Barcode.java
@@ -1,5 +1,6 @@
 package com.answer.demo.barcode;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.krysalis.barcode4j.impl.code39.Code39Bean;
 import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
@@ -7,7 +8,9 @@ import org.krysalis.barcode4j.tools.UnitConv;
 
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.URLEncoder;
 import java.util.Base64;
+import java.util.List;
 
 /**
  * @author liufeng
@@ -15,6 +18,7 @@ import java.util.Base64;
  * 条形码
  */
 public class Barcode {
+    private final static List<String> specialCodes= Lists.newArrayList("&","#","_");
 
     /**
      * 生成文件
@@ -40,8 +44,10 @@ public class Barcode {
      * @return
      */
     public static byte[] generate(String msg) {
+        String code=specialCodeEnode(msg);
+        System.out.println("特殊符号处理："+code);
         ByteArrayOutputStream ous = new ByteArrayOutputStream();
-        generate(msg, ous);
+        generate(code, ous);
         return ous.toByteArray();
     }
 
@@ -102,13 +108,26 @@ public class Barcode {
         return Base64.getEncoder().encodeToString(data);
     }
 
+    private static String specialCodeEnode(String msg){
+        String str=msg;
+        for(String code:specialCodes){
+            if(msg.contains(code)){
+                int index=msg.indexOf(code);
+                char[] chars=msg.toCharArray();
+                String specialCode=String.valueOf(chars[index]);
+                String newCode= URLEncoder.encode(specialCode);
+                str=str.replace(specialCode,newCode);
+            }
+        }
+        return str;
+    }
 
     public static void main(String[] args) {
         String msg = "moond214";
 //        String path = "barcode.png";
 //        generateFile(msg, path);
 //        System.out.println("data:image/png;base64,"+getImgStr(path));
-        byte[] bytes=generate("33332222");
+        byte[] bytes=generate("33332222&#-");
         System.out.println("data:image/png;base64,"+Base64.getEncoder().encodeToString(bytes));
     }
 }
