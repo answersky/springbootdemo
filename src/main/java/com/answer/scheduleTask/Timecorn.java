84c5e9f5068d4489bab01a866c83361b9a85a09a
liufeng <liufeng@mingdutech.com>
2022-08-12 14:12:15 +0800

    自定义注解

--- src/main/java/com/answer/scheduleTask/Timecorn.java
+++ src/main/java/com/answer/scheduleTask/Timecorn.java
@@ -12,7 +12,7 @@ import java.lang.annotation.*;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Timecorn {
     int time() default 1000;
 }
